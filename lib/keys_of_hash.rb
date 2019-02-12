81be8a0e06e69b76171515f0e901e00fa0585b45
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-12 21:18:22 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,18 +1,6 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-    arguments.each do |arg|
-      array = Hash.keys
-      array2 = Hash.values
-      i = 0
-      result = []
-      while i < array.length
-        if array2[i] == arguments
-          result.push(array[i])
-        end
-        i += 1
-      end
-    end
-    result
+    
   end
 end
