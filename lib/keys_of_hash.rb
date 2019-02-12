e39b77e4a796764bb06f0ab3f0eacfa76ae8b339
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-12 21:32:41 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,6 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-    #collect {|key, value| arguments.include?(value) ? key :nil}.compact
     array = []
     arguments.each do |var|
       self.each do |key,value|
