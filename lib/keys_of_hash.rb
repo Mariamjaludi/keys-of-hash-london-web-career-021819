f752c810f0034675f2092a07b39af90f70c01d20
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-12 20:39:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = Hash.keys 
+    array2 = Hash.values
+    i = 0
+    result = []
+    while i < array.length
+      if array2[i] == arguments
+        result.push(array[i])
+      end
+      i += 1
+    end
+        
   end
 end
