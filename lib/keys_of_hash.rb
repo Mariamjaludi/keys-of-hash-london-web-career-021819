7c12557b61bd8632a5f1920acd3e4d43bda1970d
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-12 21:35:16 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,10 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     array = []
-    arguments.each do |var|
-      self.each do |key,value|
-        if value == var
-          array.push(key)
+    arguments.each do |value|
+      self.each do |key2,value2|
+        if value2 == value
+          array.push(key2)
         end
       end
     end
